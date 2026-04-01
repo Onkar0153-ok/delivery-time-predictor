9e7bc5662b516bf27028abe171136e506dbb9075
Onkar0153-ok <onkarkurhade60@gmail.com>
2026-04-01 18:47:58 +0530

    Updated UI and added CSV upload feature

--- pages/3about.py
+++ pages/3about.py
@@ -2,6 +2,12 @@ import streamlit as st
 
 st.title("📌 About This Model")
 
+# ===== LOGO CENTERED =====
+col1, col2, col3 = st.columns([1,2,1])
+
+with col2:
+    st.image("photo.jpg",width=350)
+
 st.markdown("""
 ### 🤖 Model Used: Linear Regression
 
@@ -18,6 +24,7 @@ It predicts delivery time based on distance using the equation:
 - The relationship is linear
 
 ---
+
 
 ### 🎯 Why this model?
 - Simple and fast
